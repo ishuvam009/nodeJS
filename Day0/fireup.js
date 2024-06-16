597536f447e043519f1a38e8b0a8053e51ffbffc
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-06-16 18:48:15 +0530

    Commit

--- Day0/fireup.js
+++ Day0/fireup.js
@@ -4,7 +4,21 @@ const app = expres();
 const PORT = 3000;
 
 app.get('/',(req,res)=>{
-    console.log('Hello!');
+    res.send('Hello!');
+})
+
+//add1 to 10 function
+function add(n){
+    let ans = 0;
+    for (let i=1;i<=n;i++){
+        ans += i;
+    }
+    return ans;
+}
+
+app.get('/add',(req,res)=>{
+    const result = add(10);
+    res.send(result.toString());
 })
 
 app.listen(PORT, () => {
