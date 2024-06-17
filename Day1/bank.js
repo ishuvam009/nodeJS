eebe532cf82141d01a04bfab6330da4439a8ea38
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-06-18 01:59:01 +0530

    Commit

--- Day1/bank.js
+++ Day1/bank.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get()
+app.get('/user/', (req,res)=> {
 
-app.listen(3000);
+    //const bankId = req.query.bankid;
+    const userId = req.query.userid;
+    const password = req.query.password;
+
+    if(password == 123 && userId == 21053049){
+        res.json({
+            message: 'Your avl balance is 12,24,50,000',
+        })
+        return;
+    }else{
+        res.send('Your details are wrong plese check again.');
+        return;
+    }
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running at 3000.');
+});
